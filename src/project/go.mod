6bd5ac9150c3f6b6b62af00ccd85488d664add6d
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-07-31 21:55:26 +0900

    add project APIs

---
+++ src/project/go.mod
@@ -0,0 +1,23 @@
+module github.com/CyberAgent/src/project
+
+go 1.13
+
+require (
+	github.com/CyberAgent/mimosa-core/pkg/model v0.0.0-20200730013333-2b8d110098f3
+	github.com/CyberAgent/mimosa-core/proto/finding v0.0.0-20200729081404-67fe0858a1b8
+	github.com/CyberAgent/mimosa-core/proto/iam v0.0.0-20200730013333-2b8d110098f3
+	github.com/CyberAgent/mimosa-core/proto/project v0.0.0-20200730013333-2b8d110098f3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.4.2
+	github.com/jinzhu/gorm v1.9.15
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518 // indirect
+	github.com/stretchr/testify v1.4.0
+	github.com/vikyd/zero v0.0.0-20190921142904-0f738d0bc858
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200729003335-053ba62fc06f // indirect
+	google.golang.org/grpc v1.30.0
+)
---
+++ src/project/iam.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/CyberAgent/mimosa-core/proto/iam"
+	"github.com/kelseyhightower/envconfig"
+	"google.golang.org/grpc"
+)
+
+type iamConfig struct {
+	IAMSvcAddr string `required:"true" split_words:"true"`
+}
+
+func newIAMService() iamService {
+	var conf iamConfig
+	err := envconfig.Process("", &conf)
+	if err != nil {
+		appLogger.Fatalf("project config load error: err=%+v", err)
+	}
+	ctx := context.Background()
+	return &iamServiceImpl{
+		client: iam.NewIAMServiceClient(getGRPCConn(ctx, conf.IAMSvcAddr)),
+	}
+
+}
+
+func getGRPCConn(ctx context.Context, addr string) *grpc.ClientConn {
+	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithTimeout(time.Second*3))
+	if err != nil {
+		appLogger.Fatalf("Failed to connect backend gRPC server, addr=%s, err=%+v", addr, err)
+	}
+	return conn
+}
+
+type iamService interface {
+	CreateDefaultRole(context.Context, uint32, uint32) error
+	DeleteAllProjectRole(context.Context, uint32) error
+}
+
+type iamServiceImpl struct {
+	client iam.IAMServiceClient
+}
+
+func (i *iamServiceImpl) CreateDefaultRole(ctx context.Context, ownerUserID, projectID uint32) error {
+	policy, err := i.client.PutPolicy(ctx, &iam.PutPolicyRequest{
+		ProjectId: projectID,
+		Policy: &iam.PolicyForUpsert{
+			Name:        "project-admin",
+			ProjectId:   projectID,
+			ActionPtn:   ".*",
+			ResourcePtn: ".*",
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("Could not put default policy, err=%+v", err)
+	}
+	role, err := i.client.PutRole(ctx, &iam.PutRoleRequest{
+		ProjectId: projectID,
+		Role: &iam.RoleForUpsert{
+			Name:      "project-admin-role",
+			ProjectId: projectID,
+		},
+	})
+	if _, err := i.client.AttachPolicy(ctx, &iam.AttachPolicyRequest{
+		ProjectId: projectID,
+		RoleId:    role.Role.RoleId,
+		PolicyId:  policy.Policy.PolicyId,
+	}); err != nil {
+		return fmt.Errorf("Could not attach default policy, err=%+v", err)
+	}
+	if _, err := i.client.AttachRole(ctx, &iam.AttachRoleRequest{
+		ProjectId: projectID,
+		UserId:    ownerUserID,
+		RoleId:    role.Role.RoleId,
+	}); err != nil {
+		return fmt.Errorf("Could not attach default role, err=%+v", err)
+	}
+	return nil
+}
+
+func (i *iamServiceImpl) DeleteAllProjectRole(ctx context.Context, projectID uint32) error {
+	list, err := i.client.ListRole(ctx, &iam.ListRoleRequest{ProjectId: projectID})
+	if err != nil {
+		return err
+	}
+	for _, roleID := range list.RoleId {
+		if _, err := i.client.DeleteRole(ctx, &iam.DeleteRoleRequest{
+			ProjectId: projectID,
+			RoleId:    roleID,
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
