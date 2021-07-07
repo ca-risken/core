136709ccab229c6f53496e66d3e18223c1aefd99
mmmknt <mmmknt@mmmknt.dev>
2021-07-07 21:36:23 +0900

    Annotate env to tracing with alert

--- src/alert/main.go
+++ src/alert/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 
 	"github.com/CyberAgent/mimosa-core/proto/alert"
 	"github.com/aws/aws-xray-sdk-go/xray"
+	grpcmiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/kelseyhightower/envconfig"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type alertConf struct {
-	Port string `default:"8004"`
+	Port    string `default:"8004"`
+	EnvName string `default:"default" split_words:"true"`
 }
 
 func initXRay() {
@@ -36,7 +39,10 @@ func main() {
 	}
 
 	server := grpc.NewServer(
-		grpc.UnaryInterceptor(xray.UnaryServerInterceptor()))
+		grpc.UnaryInterceptor(
+			grpcmiddleware.ChainUnaryServer(
+				xray.UnaryServerInterceptor(),
+				annotateEnvTracingInterceptor(conf.EnvName))))
 	alertServer := newAlertService() // DI service & repository
 	alert.RegisterAlertServiceServer(server, alertServer)
 
@@ -44,5 +50,15 @@ func main() {
 	appLogger.Infof("Starting gRPC server at :%s", conf.Port)
 	if err := server.Serve(l); err != nil {
 		appLogger.Fatalf("Failed to gRPC serve: %v", err)
+	}
+}
+
+// TODO refactor
+func annotateEnvTracingInterceptor(env string) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		if err := xray.AddAnnotation(ctx, "env", env); err != nil {
+			appLogger.Warnf("failed to annotate environment to x-ray: %+v", err)
+		}
+		return handler(ctx, req)
 	}
 }
