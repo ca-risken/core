6f45e0340e98c22acace05842c171ca4d5340e84
gassara-kys <ogasawara_kiyoshi@cybernagent>
2021-07-12 11:29:02 +0900

    add analyze alert all API

--- proto/alert/service.pb.go
+++ proto/alert/service.pb.go
@@ -3696,7 +3696,7 @@ var file_alert_service_proto_rawDesc = []byte{
 	0x6a, 0x65, 0x63, 0x74, 0x49, 0x64, 0x12, 0x2c, 0x0a, 0x12, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x5f,
 	0x63, 0x6f, 0x6e, 0x64, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x69, 0x64, 0x18, 0x02, 0x20, 0x03,
 	0x28, 0x0d, 0x52, 0x10, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x43, 0x6f, 0x6e, 0x64, 0x69, 0x74, 0x69,
-	0x6f, 0x6e, 0x49, 0x64, 0x32, 0xbf, 0x18, 0x0a, 0x0c, 0x41, 0x6c, 0x65, 0x72, 0x74, 0x53, 0x65,
+	0x6f, 0x6e, 0x49, 0x64, 0x32, 0x82, 0x19, 0x0a, 0x0c, 0x41, 0x6c, 0x65, 0x72, 0x74, 0x53, 0x65,
 	0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x48, 0x0a, 0x09, 0x4c, 0x69, 0x73, 0x74, 0x41, 0x6c, 0x65,
 	0x72, 0x74, 0x12, 0x1c, 0x2e, 0x63, 0x6f, 0x72, 0x65, 0x2e, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x2e,
 	0x4c, 0x69, 0x73, 0x74, 0x41, 0x6c, 0x65, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
@@ -3892,10 +3892,15 @@ var file_alert_service_proto_rawDesc = []byte{
 	0x63, 0x6f, 0x72, 0x65, 0x2e, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x2e, 0x41, 0x6e, 0x61, 0x6c, 0x79,
 	0x7a, 0x65, 0x41, 0x6c, 0x65, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16,
 	0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66,
-	0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x2f, 0x5a, 0x2d, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62,
-	0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x43, 0x79, 0x62, 0x65, 0x72, 0x41, 0x67, 0x65, 0x6e, 0x74, 0x2f,
-	0x6d, 0x69, 0x6d, 0x6f, 0x73, 0x61, 0x2d, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x2f, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x12, 0x41, 0x0a, 0x0f, 0x41, 0x6e, 0x61, 0x6c, 0x79, 0x7a,
+	0x65, 0x41, 0x6c, 0x65, 0x72, 0x74, 0x41, 0x6c, 0x6c, 0x12, 0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67,
+	0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74,
+	0x79, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x62, 0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x2f, 0x5a, 0x2d, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x43, 0x79, 0x62, 0x65, 0x72, 0x41, 0x67, 0x65,
+	0x6e, 0x74, 0x2f, 0x6d, 0x69, 0x6d, 0x6f, 0x73, 0x61, 0x2d, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x61, 0x6c, 0x65, 0x72, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -4057,42 +4062,44 @@ var file_alert_service_proto_depIdxs = []int32{
 	54, // 64: core.alert.AlertService.PutAlertCondNotification:input_type -> core.alert.PutAlertCondNotificationRequest
 	56, // 65: core.alert.AlertService.DeleteAlertCondNotification:input_type -> core.alert.DeleteAlertCondNotificationRequest
 	57, // 66: core.alert.AlertService.AnalyzeAlert:input_type -> core.alert.AnalyzeAlertRequest
-	1,  // 67: core.alert.AlertService.ListAlert:output_type -> core.alert.ListAlertResponse
-	3,  // 68: core.alert.AlertService.GetAlert:output_type -> core.alert.GetAlertResponse
-	5,  // 69: core.alert.AlertService.PutAlert:output_type -> core.alert.PutAlertResponse
-	75, // 70: core.alert.AlertService.DeleteAlert:output_type -> google.protobuf.Empty
-	8,  // 71: core.alert.AlertService.ListAlertHistory:output_type -> core.alert.ListAlertHistoryResponse
-	10, // 72: core.alert.AlertService.GetAlertHistory:output_type -> core.alert.GetAlertHistoryResponse
-	12, // 73: core.alert.AlertService.PutAlertHistory:output_type -> core.alert.PutAlertHistoryResponse
-	75, // 74: core.alert.AlertService.DeleteAlertHistory:output_type -> google.protobuf.Empty
-	15, // 75: core.alert.AlertService.ListRelAlertFinding:output_type -> core.alert.ListRelAlertFindingResponse
-	17, // 76: core.alert.AlertService.GetRelAlertFinding:output_type -> core.alert.GetRelAlertFindingResponse
-	19, // 77: core.alert.AlertService.PutRelAlertFinding:output_type -> core.alert.PutRelAlertFindingResponse
-	75, // 78: core.alert.AlertService.DeleteRelAlertFinding:output_type -> google.protobuf.Empty
-	22, // 79: core.alert.AlertService.ListAlertCondition:output_type -> core.alert.ListAlertConditionResponse
-	24, // 80: core.alert.AlertService.GetAlertCondition:output_type -> core.alert.GetAlertConditionResponse
-	26, // 81: core.alert.AlertService.PutAlertCondition:output_type -> core.alert.PutAlertConditionResponse
-	75, // 82: core.alert.AlertService.DeleteAlertCondition:output_type -> google.protobuf.Empty
-	29, // 83: core.alert.AlertService.ListAlertRule:output_type -> core.alert.ListAlertRuleResponse
-	31, // 84: core.alert.AlertService.GetAlertRule:output_type -> core.alert.GetAlertRuleResponse
-	33, // 85: core.alert.AlertService.PutAlertRule:output_type -> core.alert.PutAlertRuleResponse
-	75, // 86: core.alert.AlertService.DeleteAlertRule:output_type -> google.protobuf.Empty
-	36, // 87: core.alert.AlertService.ListAlertCondRule:output_type -> core.alert.ListAlertCondRuleResponse
-	38, // 88: core.alert.AlertService.GetAlertCondRule:output_type -> core.alert.GetAlertCondRuleResponse
-	40, // 89: core.alert.AlertService.PutAlertCondRule:output_type -> core.alert.PutAlertCondRuleResponse
-	75, // 90: core.alert.AlertService.DeleteAlertCondRule:output_type -> google.protobuf.Empty
-	43, // 91: core.alert.AlertService.ListNotification:output_type -> core.alert.ListNotificationResponse
-	45, // 92: core.alert.AlertService.GetNotification:output_type -> core.alert.GetNotificationResponse
-	47, // 93: core.alert.AlertService.PutNotification:output_type -> core.alert.PutNotificationResponse
-	75, // 94: core.alert.AlertService.DeleteNotification:output_type -> google.protobuf.Empty
-	75, // 95: core.alert.AlertService.TestNotification:output_type -> google.protobuf.Empty
-	51, // 96: core.alert.AlertService.ListAlertCondNotification:output_type -> core.alert.ListAlertCondNotificationResponse
-	53, // 97: core.alert.AlertService.GetAlertCondNotification:output_type -> core.alert.GetAlertCondNotificationResponse
-	55, // 98: core.alert.AlertService.PutAlertCondNotification:output_type -> core.alert.PutAlertCondNotificationResponse
-	75, // 99: core.alert.AlertService.DeleteAlertCondNotification:output_type -> google.protobuf.Empty
-	75, // 100: core.alert.AlertService.AnalyzeAlert:output_type -> google.protobuf.Empty
-	67, // [67:101] is the sub-list for method output_type
-	33, // [33:67] is the sub-list for method input_type
+	75, // 67: core.alert.AlertService.AnalyzeAlertAll:input_type -> google.protobuf.Empty
+	1,  // 68: core.alert.AlertService.ListAlert:output_type -> core.alert.ListAlertResponse
+	3,  // 69: core.alert.AlertService.GetAlert:output_type -> core.alert.GetAlertResponse
+	5,  // 70: core.alert.AlertService.PutAlert:output_type -> core.alert.PutAlertResponse
+	75, // 71: core.alert.AlertService.DeleteAlert:output_type -> google.protobuf.Empty
+	8,  // 72: core.alert.AlertService.ListAlertHistory:output_type -> core.alert.ListAlertHistoryResponse
+	10, // 73: core.alert.AlertService.GetAlertHistory:output_type -> core.alert.GetAlertHistoryResponse
+	12, // 74: core.alert.AlertService.PutAlertHistory:output_type -> core.alert.PutAlertHistoryResponse
+	75, // 75: core.alert.AlertService.DeleteAlertHistory:output_type -> google.protobuf.Empty
+	15, // 76: core.alert.AlertService.ListRelAlertFinding:output_type -> core.alert.ListRelAlertFindingResponse
+	17, // 77: core.alert.AlertService.GetRelAlertFinding:output_type -> core.alert.GetRelAlertFindingResponse
+	19, // 78: core.alert.AlertService.PutRelAlertFinding:output_type -> core.alert.PutRelAlertFindingResponse
+	75, // 79: core.alert.AlertService.DeleteRelAlertFinding:output_type -> google.protobuf.Empty
+	22, // 80: core.alert.AlertService.ListAlertCondition:output_type -> core.alert.ListAlertConditionResponse
+	24, // 81: core.alert.AlertService.GetAlertCondition:output_type -> core.alert.GetAlertConditionResponse
+	26, // 82: core.alert.AlertService.PutAlertCondition:output_type -> core.alert.PutAlertConditionResponse
+	75, // 83: core.alert.AlertService.DeleteAlertCondition:output_type -> google.protobuf.Empty
+	29, // 84: core.alert.AlertService.ListAlertRule:output_type -> core.alert.ListAlertRuleResponse
+	31, // 85: core.alert.AlertService.GetAlertRule:output_type -> core.alert.GetAlertRuleResponse
+	33, // 86: core.alert.AlertService.PutAlertRule:output_type -> core.alert.PutAlertRuleResponse
+	75, // 87: core.alert.AlertService.DeleteAlertRule:output_type -> google.protobuf.Empty
+	36, // 88: core.alert.AlertService.ListAlertCondRule:output_type -> core.alert.ListAlertCondRuleResponse
+	38, // 89: core.alert.AlertService.GetAlertCondRule:output_type -> core.alert.GetAlertCondRuleResponse
+	40, // 90: core.alert.AlertService.PutAlertCondRule:output_type -> core.alert.PutAlertCondRuleResponse
+	75, // 91: core.alert.AlertService.DeleteAlertCondRule:output_type -> google.protobuf.Empty
+	43, // 92: core.alert.AlertService.ListNotification:output_type -> core.alert.ListNotificationResponse
+	45, // 93: core.alert.AlertService.GetNotification:output_type -> core.alert.GetNotificationResponse
+	47, // 94: core.alert.AlertService.PutNotification:output_type -> core.alert.PutNotificationResponse
+	75, // 95: core.alert.AlertService.DeleteNotification:output_type -> google.protobuf.Empty
+	75, // 96: core.alert.AlertService.TestNotification:output_type -> google.protobuf.Empty
+	51, // 97: core.alert.AlertService.ListAlertCondNotification:output_type -> core.alert.ListAlertCondNotificationResponse
+	53, // 98: core.alert.AlertService.GetAlertCondNotification:output_type -> core.alert.GetAlertCondNotificationResponse
+	55, // 99: core.alert.AlertService.PutAlertCondNotification:output_type -> core.alert.PutAlertCondNotificationResponse
+	75, // 100: core.alert.AlertService.DeleteAlertCondNotification:output_type -> google.protobuf.Empty
+	75, // 101: core.alert.AlertService.AnalyzeAlert:output_type -> google.protobuf.Empty
+	75, // 102: core.alert.AlertService.AnalyzeAlertAll:output_type -> google.protobuf.Empty
+	68, // [68:103] is the sub-list for method output_type
+	33, // [33:68] is the sub-list for method input_type
 	33, // [33:33] is the sub-list for extension type_name
 	33, // [33:33] is the sub-list for extension extendee
 	0,  // [0:33] is the sub-list for field type_name
@@ -4877,6 +4884,7 @@ type AlertServiceClient interface {
 	DeleteAlertCondNotification(ctx context.Context, in *DeleteAlertCondNotificationRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	// AnalyzeAlert
 	AnalyzeAlert(ctx context.Context, in *AnalyzeAlertRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	AnalyzeAlertAll(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type alertServiceClient struct {
@@ -5193,6 +5201,15 @@ func (c *alertServiceClient) AnalyzeAlert(ctx context.Context, in *AnalyzeAlertR
 	return out, nil
 }
 
+func (c *alertServiceClient) AnalyzeAlertAll(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/core.alert.AlertService/AnalyzeAlertAll", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AlertServiceServer is the server API for AlertService service.
 type AlertServiceServer interface {
 	// alert
@@ -5238,6 +5255,7 @@ type AlertServiceServer interface {
 	DeleteAlertCondNotification(context.Context, *DeleteAlertCondNotificationRequest) (*emptypb.Empty, error)
 	// AnalyzeAlert
 	AnalyzeAlert(context.Context, *AnalyzeAlertRequest) (*emptypb.Empty, error)
+	AnalyzeAlertAll(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
 }
 
 // UnimplementedAlertServiceServer can be embedded to have forward compatible implementations.
@@ -5345,6 +5363,9 @@ func (*UnimplementedAlertServiceServer) DeleteAlertCondNotification(context.Cont
 }
 func (*UnimplementedAlertServiceServer) AnalyzeAlert(context.Context, *AnalyzeAlertRequest) (*emptypb.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AnalyzeAlert not implemented")
+}
+func (*UnimplementedAlertServiceServer) AnalyzeAlertAll(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AnalyzeAlertAll not implemented")
 }
 
 func RegisterAlertServiceServer(s *grpc.Server, srv AlertServiceServer) {
@@ -5963,6 +5984,24 @@ func _AlertService_AnalyzeAlert_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _AlertService_AnalyzeAlertAll_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AlertServiceServer).AnalyzeAlertAll(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/core.alert.AlertService/AnalyzeAlertAll",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AlertServiceServer).AnalyzeAlertAll(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _AlertService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "core.alert.AlertService",
 	HandlerType: (*AlertServiceServer)(nil),
@@ -6102,6 +6141,10 @@ var _AlertService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "AnalyzeAlert",
 			Handler:    _AlertService_AnalyzeAlert_Handler,
+		},
+		{
+			MethodName: "AnalyzeAlertAll",
+			Handler:    _AlertService_AnalyzeAlertAll_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
