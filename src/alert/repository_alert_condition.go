daccdd617aad011c6ef6b38945d57d3cdbcd35e5
mmmknt <mmmknt@mmmknt.dev>
2021-07-20 20:55:00 +0900

    Add query tracing to alert

--- src/alert/repository_alert_condition.go
+++ src/alert/repository_alert_condition.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"time"
 
 	"github.com/CyberAgent/mimosa-core/pkg/model"
 	"github.com/vikyd/zero"
 )
 
-func (a *alertDB) ListAlertCondition(projectID uint32, severity []string, enabled bool, fromAt, toAt int64) (*[]model.AlertCondition, error) {
+func (a *alertDB) ListAlertCondition(ctx context.Context, projectID uint32, severity []string, enabled bool, fromAt, toAt int64) (*[]model.AlertCondition, error) {
 	query := `select * from alert_condition where project_id = ? and updated_at between ? and ?`
 	var params []interface{}
 	params = append(params, projectID, time.Unix(fromAt, 0), time.Unix(toAt, 0))
@@ -20,73 +21,73 @@ func (a *alertDB) ListAlertCondition(projectID uint32, severity []string, enable
 		params = append(params, enabled)
 	}
 	var data []model.AlertCondition
-	if err := a.Slave.Raw(query, params...).Scan(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Raw(query, params...).Scan(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) GetAlertCondition(projectID uint32, alertConditionID uint32) (*model.AlertCondition, error) {
+func (a *alertDB) GetAlertCondition(ctx context.Context, projectID uint32, alertConditionID uint32) (*model.AlertCondition, error) {
 	var data model.AlertCondition
-	if err := a.Slave.Where("project_id = ? AND alert_condition_id = ?", projectID, alertConditionID).First(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ?", projectID, alertConditionID).First(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) UpsertAlertCondition(data *model.AlertCondition) (*model.AlertCondition, error) {
+func (a *alertDB) UpsertAlertCondition(ctx context.Context, data *model.AlertCondition) (*model.AlertCondition, error) {
 	var retData model.AlertCondition
 	update := alertConditionToMap(data)
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ?", data.ProjectID, data.AlertConditionID).Assign(update).FirstOrCreate(&retData).Error; err != nil {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ?", data.ProjectID, data.AlertConditionID).Assign(update).FirstOrCreate(&retData).Error; err != nil {
 		return nil, err
 	}
 	return &retData, nil
 }
 
-func (a *alertDB) DeleteAlertCondition(projectID uint32, alertConditionID uint32) error {
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ?", projectID, alertConditionID).Delete(model.AlertCondition{}).Error; err != nil {
+func (a *alertDB) DeleteAlertCondition(ctx context.Context, projectID uint32, alertConditionID uint32) error {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ?", projectID, alertConditionID).Delete(model.AlertCondition{}).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (a *alertDB) ListAlertRule(projectID uint32, fromScore, toScore float32, fromAt, toAt int64) (*[]model.AlertRule, error) {
+func (a *alertDB) ListAlertRule(ctx context.Context, projectID uint32, fromScore, toScore float32, fromAt, toAt int64) (*[]model.AlertRule, error) {
 	query := `select * from alert_rule where project_id = ? and score between ? and ? and updated_at between ? and ?`
 	var params []interface{}
 	params = append(params, projectID, fromScore, toScore, time.Unix(fromAt, 0), time.Unix(toAt, 0))
 
 	var data []model.AlertRule
-	if err := a.Slave.Raw(query, params...).Scan(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Raw(query, params...).Scan(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) GetAlertRule(projectID uint32, alertRuleID uint32) (*model.AlertRule, error) {
+func (a *alertDB) GetAlertRule(ctx context.Context, projectID uint32, alertRuleID uint32) (*model.AlertRule, error) {
 	var data model.AlertRule
-	if err := a.Slave.Where("project_id = ? AND alert_rule_id = ?", projectID, alertRuleID).First(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Where("project_id = ? AND alert_rule_id = ?", projectID, alertRuleID).First(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) UpsertAlertRule(data *model.AlertRule) (*model.AlertRule, error) {
+func (a *alertDB) UpsertAlertRule(ctx context.Context, data *model.AlertRule) (*model.AlertRule, error) {
 	var retData model.AlertRule
 	update := alertRuleToMap(data)
-	if err := a.Master.Where("project_id = ? AND alert_rule_id = ?", data.ProjectID, data.AlertRuleID).Assign(update).FirstOrCreate(&retData).Error; err != nil {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_rule_id = ?", data.ProjectID, data.AlertRuleID).Assign(update).FirstOrCreate(&retData).Error; err != nil {
 		return nil, err
 	}
 	return &retData, nil
 }
 
-func (a *alertDB) DeleteAlertRule(projectID uint32, alertRuleID uint32) error {
-	if err := a.Master.Where("project_id = ? AND alert_rule_id = ?", projectID, alertRuleID).Delete(model.AlertRule{}).Error; err != nil {
+func (a *alertDB) DeleteAlertRule(ctx context.Context, projectID uint32, alertRuleID uint32) error {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_rule_id = ?", projectID, alertRuleID).Delete(model.AlertRule{}).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (a *alertDB) ListAlertCondRule(projectID, alertConditionID, alertRuleID uint32, fromAt, toAt int64) (*[]model.AlertCondRule, error) {
+func (a *alertDB) ListAlertCondRule(ctx context.Context, projectID, alertConditionID, alertRuleID uint32, fromAt, toAt int64) (*[]model.AlertCondRule, error) {
 	query := `select * from alert_cond_rule where project_id = ? and updated_at between ? and ?`
 	var params []interface{}
 	params = append(params, projectID, time.Unix(fromAt, 0), time.Unix(toAt, 0))
@@ -99,36 +100,36 @@ func (a *alertDB) ListAlertCondRule(projectID, alertConditionID, alertRuleID uin
 		params = append(params, alertRuleID)
 	}
 	var data []model.AlertCondRule
-	if err := a.Slave.Raw(query, params...).Scan(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Raw(query, params...).Scan(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) GetAlertCondRule(projectID, alertConditionID, alertRuleID uint32) (*model.AlertCondRule, error) {
+func (a *alertDB) GetAlertCondRule(ctx context.Context, projectID, alertConditionID, alertRuleID uint32) (*model.AlertCondRule, error) {
 	var data model.AlertCondRule
-	if err := a.Slave.Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", projectID, alertConditionID, alertRuleID).First(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", projectID, alertConditionID, alertRuleID).First(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) UpsertAlertCondRule(data *model.AlertCondRule) (*model.AlertCondRule, error) {
+func (a *alertDB) UpsertAlertCondRule(ctx context.Context, data *model.AlertCondRule) (*model.AlertCondRule, error) {
 	var retData model.AlertCondRule
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", data.ProjectID, data.AlertConditionID, data.AlertRuleID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", data.ProjectID, data.AlertConditionID, data.AlertRuleID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
 		return nil, err
 	}
 	return &retData, nil
 }
 
-func (a *alertDB) DeleteAlertCondRule(projectID, alertConditionID, alertRuleID uint32) error {
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", projectID, alertConditionID, alertRuleID).Delete(model.AlertCondRule{}).Error; err != nil {
+func (a *alertDB) DeleteAlertCondRule(ctx context.Context, projectID, alertConditionID, alertRuleID uint32) error {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND alert_rule_id = ?", projectID, alertConditionID, alertRuleID).Delete(model.AlertCondRule{}).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (a *alertDB) ListNotification(projectID uint32, notifyType string, fromAt, toAt int64) (*[]model.Notification, error) {
+func (a *alertDB) ListNotification(ctx context.Context, projectID uint32, notifyType string, fromAt, toAt int64) (*[]model.Notification, error) {
 	query := `select * from notification where project_id = ? and updated_at between ? and ?`
 	var params []interface{}
 	params = append(params, projectID, time.Unix(fromAt, 0), time.Unix(toAt, 0))
@@ -137,36 +138,36 @@ func (a *alertDB) ListNotification(projectID uint32, notifyType string, fromAt, 
 		params = append(params, notifyType)
 	}
 	var data []model.Notification
-	if err := a.Slave.Raw(query, params...).Scan(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Raw(query, params...).Scan(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) GetNotification(projectID uint32, NotificationID uint32) (*model.Notification, error) {
+func (a *alertDB) GetNotification(ctx context.Context, projectID uint32, NotificationID uint32) (*model.Notification, error) {
 	var data model.Notification
-	if err := a.Slave.Where("project_id = ? AND notification_id = ?", projectID, NotificationID).First(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Where("project_id = ? AND notification_id = ?", projectID, NotificationID).First(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) UpsertNotification(data *model.Notification) (*model.Notification, error) {
+func (a *alertDB) UpsertNotification(ctx context.Context, data *model.Notification) (*model.Notification, error) {
 	var retData model.Notification
-	if err := a.Master.Where("project_id = ? AND notification_id = ?", data.ProjectID, data.NotificationID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND notification_id = ?", data.ProjectID, data.NotificationID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
 		return nil, err
 	}
 	return &retData, nil
 }
 
-func (a *alertDB) DeleteNotification(projectID uint32, NotificationID uint32) error {
-	if err := a.Master.Where("project_id = ? AND notification_id = ?", projectID, NotificationID).Delete(model.Notification{}).Error; err != nil {
+func (a *alertDB) DeleteNotification(ctx context.Context, projectID uint32, NotificationID uint32) error {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND notification_id = ?", projectID, NotificationID).Delete(model.Notification{}).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (a *alertDB) ListAlertCondNotification(projectID, alertConditionID, notificationID uint32, fromAt, toAt int64) (*[]model.AlertCondNotification, error) {
+func (a *alertDB) ListAlertCondNotification(ctx context.Context, projectID, alertConditionID, notificationID uint32, fromAt, toAt int64) (*[]model.AlertCondNotification, error) {
 	query := `select * from alert_cond_notification where project_id = ? and updated_at between ? and ?`
 	var params []interface{}
 	params = append(params, projectID, time.Unix(fromAt, 0), time.Unix(toAt, 0))
@@ -179,30 +180,30 @@ func (a *alertDB) ListAlertCondNotification(projectID, alertConditionID, notific
 		params = append(params, notificationID)
 	}
 	var data []model.AlertCondNotification
-	if err := a.Slave.Raw(query, params...).Scan(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Raw(query, params...).Scan(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) GetAlertCondNotification(projectID, alertConditionID, notificationID uint32) (*model.AlertCondNotification, error) {
+func (a *alertDB) GetAlertCondNotification(ctx context.Context, projectID, alertConditionID, notificationID uint32) (*model.AlertCondNotification, error) {
 	var data model.AlertCondNotification
-	if err := a.Slave.Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", projectID, alertConditionID, notificationID).First(&data).Error; err != nil {
+	if err := a.Slave.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", projectID, alertConditionID, notificationID).First(&data).Error; err != nil {
 		return nil, err
 	}
 	return &data, nil
 }
 
-func (a *alertDB) UpsertAlertCondNotification(data *model.AlertCondNotification) (*model.AlertCondNotification, error) {
+func (a *alertDB) UpsertAlertCondNotification(ctx context.Context, data *model.AlertCondNotification) (*model.AlertCondNotification, error) {
 	var retData model.AlertCondNotification
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", data.ProjectID, data.AlertConditionID, data.NotificationID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", data.ProjectID, data.AlertConditionID, data.NotificationID).Assign(data).FirstOrCreate(&retData).Error; err != nil {
 		return nil, err
 	}
 	return &retData, nil
 }
 
-func (a *alertDB) DeleteAlertCondNotification(projectID, alertConditionID, notificationID uint32) error {
-	if err := a.Master.Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", projectID, alertConditionID, notificationID).Delete(model.AlertCondNotification{}).Error; err != nil {
+func (a *alertDB) DeleteAlertCondNotification(ctx context.Context, projectID, alertConditionID, notificationID uint32) error {
+	if err := a.Master.WithContext(ctx).Where("project_id = ? AND alert_condition_id = ? AND notification_id = ?", projectID, alertConditionID, notificationID).Delete(model.AlertCondNotification{}).Error; err != nil {
 		return err
 	}
 	return nil
--- src/alert/service_test.go
+++ src/alert/service_test.go
@@ -19,177 +19,177 @@ type mockAlertRepository struct {
 
 // Alert
 
-func (m *mockAlertRepository) ListAlert(uint32, []string, []string, string, int64, int64) (*[]model.Alert, error) {
+func (m *mockAlertRepository) ListAlert(context.Context, uint32, []string, []string, string, int64, int64) (*[]model.Alert, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.Alert), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlert(uint32, uint32) (*model.Alert, error) {
+func (m *mockAlertRepository) GetAlert(context.Context, uint32, uint32) (*model.Alert, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Alert), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertByAlertConditionID(uint32, uint32) (*model.Alert, error) {
+func (m *mockAlertRepository) GetAlertByAlertConditionID(context.Context, uint32, uint32) (*model.Alert, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Alert), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlert(*model.Alert) (*model.Alert, error) {
+func (m *mockAlertRepository) UpsertAlert(context.Context, *model.Alert) (*model.Alert, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Alert), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlert(uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlert(context.Context, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListAlertHistory(uint32, uint32, []string, []string, int64, int64) (*[]model.AlertHistory, error) {
+func (m *mockAlertRepository) ListAlertHistory(context.Context, uint32, uint32, []string, []string, int64, int64) (*[]model.AlertHistory, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertHistory), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertHistory(uint32, uint32) (*model.AlertHistory, error) {
+func (m *mockAlertRepository) GetAlertHistory(context.Context, uint32, uint32) (*model.AlertHistory, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertHistory), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlertHistory(*model.AlertHistory) (*model.AlertHistory, error) {
+func (m *mockAlertRepository) UpsertAlertHistory(context.Context, *model.AlertHistory) (*model.AlertHistory, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertHistory), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlertHistory(uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlertHistory(context.Context, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListRelAlertFinding(uint32, uint32, uint32, int64, int64) (*[]model.RelAlertFinding, error) {
+func (m *mockAlertRepository) ListRelAlertFinding(context.Context, uint32, uint32, uint32, int64, int64) (*[]model.RelAlertFinding, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.RelAlertFinding), args.Error(1)
 }
-func (m *mockAlertRepository) GetRelAlertFinding(uint32, uint32, uint32) (*model.RelAlertFinding, error) {
+func (m *mockAlertRepository) GetRelAlertFinding(context.Context, uint32, uint32, uint32) (*model.RelAlertFinding, error) {
 	args := m.Called()
 	return args.Get(0).(*model.RelAlertFinding), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertRelAlertFinding(*model.RelAlertFinding) (*model.RelAlertFinding, error) {
+func (m *mockAlertRepository) UpsertRelAlertFinding(context.Context, *model.RelAlertFinding) (*model.RelAlertFinding, error) {
 	args := m.Called()
 	return args.Get(0).(*model.RelAlertFinding), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteRelAlertFinding(uint32, uint32, uint32) error {
+func (m *mockAlertRepository) DeleteRelAlertFinding(context.Context, uint32, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListAlertCondition(uint32, []string, bool, int64, int64) (*[]model.AlertCondition, error) {
+func (m *mockAlertRepository) ListAlertCondition(context.Context, uint32, []string, bool, int64, int64) (*[]model.AlertCondition, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertCondition), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertCondition(uint32, uint32) (*model.AlertCondition, error) {
+func (m *mockAlertRepository) GetAlertCondition(context.Context, uint32, uint32) (*model.AlertCondition, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondition), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlertCondition(*model.AlertCondition) (*model.AlertCondition, error) {
+func (m *mockAlertRepository) UpsertAlertCondition(context.Context, *model.AlertCondition) (*model.AlertCondition, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondition), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlertCondition(uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlertCondition(context.Context, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListAlertRule(uint32, float32, float32, int64, int64) (*[]model.AlertRule, error) {
+func (m *mockAlertRepository) ListAlertRule(context.Context, uint32, float32, float32, int64, int64) (*[]model.AlertRule, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertRule), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertRule(uint32, uint32) (*model.AlertRule, error) {
+func (m *mockAlertRepository) GetAlertRule(context.Context, uint32, uint32) (*model.AlertRule, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertRule), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlertRule(*model.AlertRule) (*model.AlertRule, error) {
+func (m *mockAlertRepository) UpsertAlertRule(context.Context, *model.AlertRule) (*model.AlertRule, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertRule), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlertRule(uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlertRule(context.Context, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListAlertCondRule(uint32, uint32, uint32, int64, int64) (*[]model.AlertCondRule, error) {
+func (m *mockAlertRepository) ListAlertCondRule(context.Context, uint32, uint32, uint32, int64, int64) (*[]model.AlertCondRule, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertCondRule), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertCondRule(uint32, uint32, uint32) (*model.AlertCondRule, error) {
+func (m *mockAlertRepository) GetAlertCondRule(context.Context, uint32, uint32, uint32) (*model.AlertCondRule, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondRule), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlertCondRule(*model.AlertCondRule) (*model.AlertCondRule, error) {
+func (m *mockAlertRepository) UpsertAlertCondRule(context.Context, *model.AlertCondRule) (*model.AlertCondRule, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondRule), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlertCondRule(uint32, uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlertCondRule(context.Context, uint32, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListNotification(uint32, string, int64, int64) (*[]model.Notification, error) {
+func (m *mockAlertRepository) ListNotification(context.Context, uint32, string, int64, int64) (*[]model.Notification, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.Notification), args.Error(1)
 }
-func (m *mockAlertRepository) GetNotification(uint32, uint32) (*model.Notification, error) {
+func (m *mockAlertRepository) GetNotification(context.Context, uint32, uint32) (*model.Notification, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Notification), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertNotification(*model.Notification) (*model.Notification, error) {
+func (m *mockAlertRepository) UpsertNotification(context.Context, *model.Notification) (*model.Notification, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Notification), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteNotification(uint32, uint32) error {
+func (m *mockAlertRepository) DeleteNotification(context.Context, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) ListAlertCondNotification(uint32, uint32, uint32, int64, int64) (*[]model.AlertCondNotification, error) {
+func (m *mockAlertRepository) ListAlertCondNotification(context.Context, uint32, uint32, uint32, int64, int64) (*[]model.AlertCondNotification, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertCondNotification), args.Error(1)
 }
-func (m *mockAlertRepository) GetAlertCondNotification(uint32, uint32, uint32) (*model.AlertCondNotification, error) {
+func (m *mockAlertRepository) GetAlertCondNotification(context.Context, uint32, uint32, uint32) (*model.AlertCondNotification, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondNotification), args.Error(1)
 }
-func (m *mockAlertRepository) UpsertAlertCondNotification(*model.AlertCondNotification) (*model.AlertCondNotification, error) {
+func (m *mockAlertRepository) UpsertAlertCondNotification(context.Context, *model.AlertCondNotification) (*model.AlertCondNotification, error) {
 	args := m.Called()
 	return args.Get(0).(*model.AlertCondNotification), args.Error(1)
 }
-func (m *mockAlertRepository) DeleteAlertCondNotification(uint32, uint32, uint32) error {
+func (m *mockAlertRepository) DeleteAlertCondNotification(context.Context, uint32, uint32, uint32) error {
 	args := m.Called()
 	return args.Error(0)
 }
 
-func (m *mockAlertRepository) ListAlertRuleByAlertConditionID(uint32, uint32) (*[]model.AlertRule, error) {
+func (m *mockAlertRepository) ListAlertRuleByAlertConditionID(context.Context, uint32, uint32) (*[]model.AlertRule, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertRule), args.Error(1)
 }
-func (m *mockAlertRepository) ListNotificationByAlertConditionID(uint32, uint32) (*[]model.Notification, error) {
+func (m *mockAlertRepository) ListNotificationByAlertConditionID(context.Context, uint32, uint32) (*[]model.Notification, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.Notification), args.Error(1)
 }
-func (m *mockAlertRepository) DeactivateAlert(*model.Alert) error {
+func (m *mockAlertRepository) DeactivateAlert(context.Context, *model.Alert) error {
 	args := m.Called()
 	return args.Error(0)
 }
-func (m *mockAlertRepository) GetAlertByAlertConditionIDStatus(uint32, uint32, []string) (*model.Alert, error) {
+func (m *mockAlertRepository) GetAlertByAlertConditionIDStatus(context.Context, uint32, uint32, []string) (*model.Alert, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Alert), args.Error(1)
 }
 
-func (m *mockAlertRepository) ListFinding(uint32) (*[]model.Finding, error) {
+func (m *mockAlertRepository) ListFinding(context.Context, uint32) (*[]model.Finding, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.Finding), args.Error(1)
 }
 
-func (m *mockAlertRepository) ListFindingTag(uint32, uint64) (*[]model.FindingTag, error) {
+func (m *mockAlertRepository) ListFindingTag(context.Context, uint32, uint64) (*[]model.FindingTag, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.FindingTag), args.Error(1)
 }
 
-func (m *mockAlertRepository) ListEnabledAlertCondition(uint32, []uint32) (*[]model.AlertCondition, error) {
+func (m *mockAlertRepository) ListEnabledAlertCondition(context.Context, uint32, []uint32) (*[]model.AlertCondition, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertCondition), args.Error(1)
 }
 
-func (m *mockAlertRepository) ListDisabledAlertCondition(uint32, []uint32) (*[]model.AlertCondition, error) {
+func (m *mockAlertRepository) ListDisabledAlertCondition(context.Context, uint32, []uint32) (*[]model.AlertCondition, error) {
 	args := m.Called()
 	return args.Get(0).(*[]model.AlertCondition), args.Error(1)
 }
 
-func (m *mockAlertRepository) GetProject(uint32) (*model.Project, error) {
+func (m *mockAlertRepository) GetProject(context.Context, uint32) (*model.Project, error) {
 	args := m.Called()
 	return args.Get(0).(*model.Project), args.Error(1)
 }
