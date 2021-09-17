d43e30a1570f523bb92e968f658013315bae1d13
gassara-kys <ogasawara_kiyoshi@cybernagent>
2021-09-17 20:19:01 +0900

    add Authz for admin

--- proto/iam/validator.go
+++ proto/iam/validator.go
@@ -277,6 +277,17 @@ func (i *IsAuthorizedRequest) Validate() error {
 	)
 }
 
+// Validate IsAuthorizedAdminRequest
+func (i *IsAuthorizedAdminRequest) Validate() error {
+	return validation.ValidateStruct(i,
+		validation.Field(&i.UserId, validation.Required),
+		// must format: "<service-name>/<action-name>"
+		validation.Field(&i.ActionName, validation.Required, validation.Match(regexp.MustCompile(`^(\w|-)+/(\w|-)+$`))),
+		// must format: "<prefix>/<prefix>/.../<resource-name>"
+		validation.Field(&i.ResourceName, validation.Required, validation.Match(regexp.MustCompile(`^(\w|-|:|/)+/.+$`))),
+	)
+}
+
 // Validate IsAuthorizedTokenRequest
 func (i *IsAuthorizedTokenRequest) Validate() error {
 	return validation.ValidateStruct(i,
