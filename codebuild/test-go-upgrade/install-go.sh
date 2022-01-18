d8c26914d54077cae26933c765584eef94f91d31
mmmknt <mmmknt@mmmknt.dev>
2022-01-18 11:41:12 +0900

    WIP

--- codebuild/test-go-upgrade/install-go.sh
+++ codebuild/test-go-upgrade/install-go.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+INSTALL_GO_VERSION=1.17.1
 if [[ "${ARCH}" = "" ]]; then
   echo "environment value ARCH is not set"
   exit 1
@@ -6,9 +7,9 @@ fi
 
 
 if [[ "${ARCH}" = "arm64" ]]; then
-  curl -OL https://go.dev/dl/go1.17.1.linux-arm64.tar.gz
-  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-arm64.tar.gz
+  curl -OL "https://go.dev/dl/go${INSTALL_GO_VERSION}.linux-arm64.tar.gz"
+  rm -rf /usr/local/go && tar -C /usr/local -xzf "go${INSTALL_GO_VERSION}.linux-arm64.tar.gz"
 else
-  goenv install 1.17.1
-  goenv global 1.17.1
+  goenv install ${INSTALL_GO_VERSION}
+  goenv global ${INSTALL_GO_VERSION}
 fi
