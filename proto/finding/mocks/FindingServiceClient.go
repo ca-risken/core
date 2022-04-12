9a70b62f168ba55ecaa554a9f104a496fde0763b
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-04-12 12:22:08 +0900

    add: put finding batch

--- proto/finding/mocks/FindingServiceClient.go
+++ proto/finding/mocks/FindingServiceClient.go
@@ -588,6 +588,36 @@ func (_m *FindingServiceClient) PutFinding(ctx context.Context, in *finding.PutF
 	return r0, r1
 }
 
+// PutFindingBatch provides a mock function with given fields: ctx, in, opts
+func (_m *FindingServiceClient) PutFindingBatch(ctx context.Context, in *finding.PutFindingBatchRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *emptypb.Empty
+	if rf, ok := ret.Get(0).(func(context.Context, *finding.PutFindingBatchRequest, ...grpc.CallOption) *emptypb.Empty); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*emptypb.Empty)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *finding.PutFindingBatchRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PutFindingSetting provides a mock function with given fields: ctx, in, opts
 func (_m *FindingServiceClient) PutFindingSetting(ctx context.Context, in *finding.PutFindingSettingRequest, opts ...grpc.CallOption) (*finding.PutFindingSettingResponse, error) {
 	_va := make([]interface{}, len(opts))
