aa47287771fb4a9b295b154a82775c0866e7e314
Senk <44151180+senk8@users.noreply.github.com>
2026-04-08 11:22:18 +0900

    fix: remove unnecessary action patterns from viewer role defaults

--- pkg/server/organization/organization.go
+++ pkg/server/organization/organization.go
@@ -192,7 +192,7 @@ func (o *OrganizationService) createDefaultRole(ctx context.Context, ownerUserID
 	organizationAdmin := "organization-admin"
 	organizationEditor := "organization-editor"
 	organizationViewer := "organization-viewer"
-	viewerActionPtn := "get|list|is-admin|put-alert-first-viewed-at"
+	viewerActionPtn := "get|list"
 	editorActionPtn := viewerActionPtn + "|organization/update-organization|project/update-project|project/tag-project|project/untag-project|organization-alert/.*|finding/.*|alert/.*|report/.*|aws/.*|google/.*|azure/.*|osint/.*|diagnosis/.*|code/.*|datasource/.*|ai/.*"
 
 	for name, actionPtn := range map[string]string{
